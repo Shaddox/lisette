42805bdf805bb988d8fb6198be719769a82b8714
Shaddox <sorin.gears@gmail.com>
2017-09-26 17:28:14 +0300

    Initial commit

---
+++ start.js
@@ -0,0 +1,15 @@
+const Discord = require("discord.js");
+const client = new Discord.Client();
+const config = require("./config/config.json");
+
+client.on("ready", () => {
+    console.log("I am ready!");
+});
+
+client.on("message", (message) => {
+    if (message.content.startsWith(config.prefix + "ping")) {
+    message.channel.send("pong!");
+}
+});
+
+client.login(config.token);
