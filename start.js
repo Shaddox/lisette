4b31461502765820ef1ecf80d0630cfec8360eb7
Shaddox <sorin.gears@gmail.com>
2017-09-26 19:31:49 +0300

    More configs

--- start.js
+++ start.js
@@ -1,3 +1,6 @@
+if (process.version.slice(1).split(".")[0] < 8)
+    throw new Error("Node 8.0.0 or higher is required. Update Node on your system.");
+
 const Discord = require("discord.js");
 const client = new Discord.Client();
 const config = require("./config/config.json");
